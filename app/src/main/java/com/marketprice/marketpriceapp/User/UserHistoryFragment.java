980734da34027bdc4e149a866ff2719578fc69f3
Indra Prasetya <IrvineStuck9090*>
2018-02-04 01:30:08 +0700

    oke

--- app/src/main/java/com/marketprice/marketpriceapp/User/UserHistoryFragment.java
+++ app/src/main/java/com/marketprice/marketpriceapp/User/UserHistoryFragment.java
@@ -26,17 +26,6 @@ public class UserHistoryFragment extends Fragment {
     private HistoryAdapter historyAdapter;
     private List<History> listhistory = new ArrayList<History>();
     RecyclerView recyclerViewHistory;
-
-    private BarangAdapter barangAdapter;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private List<Barang> listBarang = new ArrayList<Barang>();
-=======
-    private List<Barang> listbarang = new ArrayList<Barang>();
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-=======
-    private List<Barang> listbarang = new ArrayList<Barang>();
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
     RecyclerView recyclerViewBarang;
 
     public UserHistoryFragment() {
@@ -45,71 +34,9 @@ public class UserHistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-//        View view = inflater.inflate(R.layout.fragment_user_history, container, false);
-        View view = inflater.inflate(R.layout.activity_menu, container, false);
-
-        /*recyclerViewHistory = (RecyclerView) view.findViewById(R.id.RVBarang);
-=======
-        /*View view = inflater.inflate(R.layout.fragment_user_history, container, false);
-
-        recyclerViewHistory = (RecyclerView) view.findViewById(R.id.RVHistoryPurchase);
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-=======
-        /*View view = inflater.inflate(R.layout.fragment_user_history, container, false);
-
-        recyclerViewHistory = (RecyclerView) view.findViewById(R.id.RVHistoryPurchase);
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-        recyclerViewHistory.setHasFixedSize(true);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(view.getContext());
-        recyclerViewHistory.setLayoutManager(layoutManager);
-        addDummyHistory();
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-        historyAdapter = new HistoryAdapter(getContext(), listhistory, recyclerViewHistory);
-        recyclerViewHistory.setAdapter(historyAdapter);*/
-
-        recyclerViewBarang = (RecyclerView) view.findViewById(R.id.RVBarang);
-        recyclerViewBarang.setHasFixedSize(true);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerViewBarang.setLayoutManager(layoutManager);
-        addDummyBarang();
-
-        barangAdapter = new BarangAdapter(listBarang, recyclerViewBarang, getContext());
-        recyclerViewBarang.setAdapter(barangAdapter);
-
-        /*recyclerViewHistory.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), new RecyclerItemClickListener.OnItemClickListener() {
-=======
-=======
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-        historyAdapter = new HistoryAdapter(listhistory, recyclerViewHistory, view.getContext());
-        recyclerViewHistory.setAdapter(historyAdapter);
-        recyclerViewHistory.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), new RecyclerItemClickListener.OnItemClickListener() {
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-            @Override
-            public void onItemClick(View view, int position) {
-                Intent i = new Intent(view.getContext(), HistoryDetailActivity.class);
-                i.putExtra("historyName", listhistory.get(position).getHargaBarangPurchase());
-                i.putExtra("historyId", listhistory.get(position).getIdHistory().toString());
-                i.putExtra("historyPrice", listhistory.get(position).getHargaBarangPurchase().toString());
-                i.putExtra("historyDesc", listhistory.get(position).getAdditionalMessagePurchase().toString());
-                startActivity(i);
-            }
-        }));*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-
         View view = inflater.inflate(R.layout.fragment_user_history, container, false);
 
-        recyclerViewHistory= (RecyclerView) view.findViewById(R.id.RVHistoryPurchase);
+        recyclerViewHistory = (RecyclerView) view.findViewById(R.id.RVHistoryPurchase);
         recyclerViewHistory.setHasFixedSize(true);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
@@ -118,12 +45,6 @@ public class UserHistoryFragment extends Fragment {
 
         historyAdapter = new HistoryAdapter(listhistory, recyclerViewHistory, getContext());
         recyclerViewHistory.setAdapter(historyAdapter);
-
-<<<<<<< HEAD
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-=======
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-
         return view;
 
 
@@ -140,28 +61,6 @@ public class UserHistoryFragment extends Fragment {
             data.setPurchaceDate(null);
             listhistory.add(data);
             Log.v("List History", "Ini Masuk data ke " + i);
-            j++;
-        }
-    }
-
-    private void addDummyBarang() {
-        int j = 1;
-        for (int i = 0; i < 20; i++) {
-            Barang data = new Barang();
-            data.setBarangId(Long.valueOf(j));
-            data.setBarangName("Barang " + j);
-            data.setBarangPrice(j * 1000);
-            data.setBarangValue(j * 10);
-<<<<<<< HEAD
-<<<<<<< HEAD
-            listBarang.add(data);
-=======
-            listbarang.add(data);
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-=======
-            listbarang.add(data);
->>>>>>> c3c3ac9bd2aeabc0f6b4cc43069aaf2eb865cbb3
-            Log.v("List Barang", "Ini Masuk data ke " + i);
             j++;
         }
     }
